089a619710b29926ad2b8495cb06bf923cd7d158
ETU1340 <noumsfinoana@gmail.com>
2021-08-25 01:34:59 +0300

    erreur template

--- Exam24h/application/views/index.php
+++ Exam24h/application/views/index.php
@@ -14,48 +14,38 @@ $Acueil="Acceuil.php"
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no">
   <meta name="apple-mobile-web-app-capable" content="yes">
-  <link href="apple-touch-icon.png" rel="apple-touch-icon">
-  <link href="favicon.png" rel="icon">
+  <link href=<?php echo img_loader("apple-touch-icon","png");?> rel="apple-touch-icon">
+  <link href="<?php echo img_loader("favicon.png","png");?> rel="icon">
   <meta name="author" content="Nghia Minh Luong">
   <meta name="keywords" content="Default Description">
   <meta name="description" content="Default keyword">
   <title>Sky - Homepage</title>
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo css_loader("font-awesome.min.css"); ?>" rel="stylesheet">
-    <link href="<?php echo css_loader("style.css"); ?>" rel="stylesheet">
-    <link href="<?php echo css_loader("bootstrap.min.css"); ?>" rel="stylesheet">
-    <link href="<?php echo css_loader("font-awesome.min.css"); ?>" rel="stylesheet">
-    <link href="<?php echo css_loader("font-awesome.min.css"); ?>" rel="stylesheet">
-    <link href="<?php echo css_loader("font-awesome.min.css"); ?>" rel="stylesheet">
-    <link href="<?php echo css_loader("font-awesome.min.css"); ?>" rel="stylesheet">
-    <link href="<?php echo css_loader("font-awesome.min.css"); ?>" rel="stylesheet">
-    <link href="<?php echo css_loader("font-awesome.min.css"); ?>" rel="stylesheet">
-    <link href="<?php echo css_loader("font-awesome.min.css"); ?>" rel="stylesheet">
+    <link href="<?php echo css_loader('font-awesome.min'); ?>" rel="stylesheet">
+    <link href="<?php echo css_loader('style'); ?>" rel="stylesheet">
+    <link href="<?php echo css_loader('bootstrap.min'); ?>" rel="stylesheet">
+    <link href="<?php echo css_loader('owl.carousel'); ?>" rel="stylesheet">
+    <link href="<?php echo css_loader('fontawesome-stars'); ?>" rel="stylesheet">
+    <link href="<?php echo css_loader('bootstrap-select.min'); ?>" rel="stylesheet">
+    <link href="<?php echo css_loader('slick'); ?>" rel="stylesheet">
+    <link href="<?php echo css_loader('magnific-popup'); ?>" rel="stylesheet">
+    <link href="<?php echo css_loader('jquery-ui.min'); ?>" rel="stylesheet">
+    <link href="<?php echo css_loader('settings'); ?>" rel="stylesheet">
+    <link href="<?php echo css_loader('layers'); ?>" rel="stylesheet">
+    <link href="<?php echo css_loader('navigation'); ?>" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="<?php echo css_loader("shop-homepage"); ?>" rel="stylesheet">
+    <link href="<?php echo css_loader('style1'); ?>" rel="stylesheet">
 
   <!-- Fonts-->
   <!-- link href="https://fonts.googleapis.com/css?family=Archivo+Narrow:300,400,700%7CMontserrat:300,400,500,600,700,800,900" rel="stylesheet" -->
-  <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
-  <link rel="stylesheet" href="plugins/ps-icon/style.css">
-  <!-- CSS Library-->
-  <link rel="stylesheet" href="plugins/bootstrap/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="plugins/owl-carousel/assets/owl.carousel.css">
-  <link rel="stylesheet" href="plugins/jquery-bar-rating/dist/themes/fontawesome-stars.css">
-  <link rel="stylesheet" href="plugins/slick/slick/slick.css">
-  <link rel="stylesheet" href="plugins/bootstrap-select/dist/css/bootstrap-select.min.css">
-  <link rel="stylesheet" href="plugins/Magnific-Popup/dist/magnific-popup.css">
-  <link rel="stylesheet" href="plugins/jquery-ui/jquery-ui.min.css">
-  <link rel="stylesheet" href="plugins/revolution/css/settings.css">
-  <link rel="stylesheet" href="plugins/revolution/css/layers.css">
-  <link rel="stylesheet" href="plugins/revolution/css/navigation.css">
+
   <!-- Custom-->
-  <link rel="stylesheet" href="css/style.css">
+
 
   <!--HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries-->
   <!--WARNING: Respond.js doesn't work if you view the page via file://-->
-  <!--[if lt IE 9]><script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script><script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
+  <!--[if lt IE 9]><script   <script <?php echo js_loader("https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv")?> ></script><script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
 </head>
 <!--[if IE 7]><body class="ie7 lt-ie8 lt-ie9 lt-ie10"><![endif]-->
 <!--[if IE 8]><body class="ie8 lt-ie9 lt-ie10"><![endif]-->
@@ -99,33 +89,32 @@ $Acueil="Acceuil.php"
 
    
        <!-- JS Library-->
-  <script type="text/javascript" src="plugins/jquery/dist/jquery.min.js"></script>
-  <script type="text/javascript" src="plugins/bootstrap/dist/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="plugins/jquery-bar-rating/dist/jquery.barrating.min.js"></script>
-  <script type="text/javascript" src="plugins/owl-carousel/owl.carousel.min.js"></script>
-  <script type="text/javascript" src="plugins/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
-<script type="text/javascript" src="plugins/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+  <script  type="text/javascript" <?php echo js_loader("jquery.min"); ?>></script>
+  <script  type="text/javascript"<?php echo js_loader("bootstrap.min"); ?>></script>
+  <script  type="text/javascript" <?php echo js_loader("jquery.barrating.min"); ?>></script>
+  <script  type="text/javascript" <?php echo js_loader("owl.carousel.min.js"); ?>></script>
+  <script <?php echo js_loader("revolution.extension.kenburn.min"); ?></script>
+  <script <?php echo js_loader("revolution.extension.migration.min"); ?>></script>
   <!--script type="text/javascript" src="plugins/gmap3.min.js"></script-->
-  <script type="text/javascript" src="plugins/imagesloaded.pkgd.js"></script>
-  <script type="text/javascript" src="plugins/isotope.pkgd.min.js"></script>
-  <script type="text/javascript" src="plugins/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
-  <script type="text/javascript" src="plugins/jquery.matchHeight-min.js"></script>
-  <script type="text/javascript" src="plugins/slick/slick/slick.min.js"></script>
-  <script type="text/javascript" src="plugins/elevatezoom/jquery.elevatezoom.js"></script>
-  <script type="text/javascript" src="plugins/Magnific-Popup/dist/jquery.magnific-popup.min.js"></script>
-  <script type="text/javascript" src="plugins/jquery-ui/jquery-ui.min.js"></script>
-  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAx39JFH5nhxze1ZydH-Kl8xXM3OK4fvcg&amp;region=GB"></script>
-  <script type="text/javascript" src="plugins/revolution/js/jquery.themepunch.tools.min.js"></script>
-  <script type="text/javascript" src="plugins/revolution/js/jquery.themepunch.revolution.min.js"></script>
-  <script type="text/javascript" src="plugins/revolution/js/extensions/revolution.extension.video.min.js"></script>
-  <script type="text/javascript" src="plugins/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
-  <script type="text/javascript"
-    src="plugins/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
-  <script type="text/javascript" src="plugins/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
-  <script type="text/javascript" src="plugins/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
-  <script type="text/javascript" src="plugins/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+  <script <?php echo js_loader("imagesloaded.pkgd"); ?>></script>
+  <script <?php echo js_loader("isotope.pkgd.min"); ?>></script>
+  <script <?php echo js_loader("bootstrap-select.min"); ?>></script>
+  <script <?php echo js_loader("jquery.matchHeight-min"); ?>></script>
+  <script <?php echo js_loader("slick.min"); ?>></script>
+  <script <?php echo js_loader("jquery.elevatezoom"); ?>></script>
+  <script <?php echo js_loader("jquery.magnific-popup.min"); ?>></script>
+  <script <?php echo js_loader("jquery-ui.min"); ?>></script>
+  <script  <?php echo js_loader("https://maps.googleapis.com/maps/api/js?key=AIzaSyAx39JFH5nhxze1ZydH-Kl8xXM3OK4fvcg&amp;region=GB"); ?>></script>
+  <script  <?php echo js_loader("jquery.themepunch.tools.min"); ?>></script>
+  <script  <?php echo js_loader("jquery.themepunch.revolution.min") ; ?>></script>
+  <script  <?php echo js_loader("revolution.extension.video.min"); ?>></script>
+  <script  <?php echo js_loader("revolution.extension.slideanims.min"); ?>></script>
+  <script  <?php echo js_loader("revolution.extension.layeranimation.min"); ?>></script>
+  <script <?php echo js_loader("revolution.extension.navigation.min"); ?>></script>
+  <script <?php echo js_loader("revolution.extension.parallax.min"); ?>></script>
+  <script  <?php echo js_loader("revolution.extension.actions.min"); ?>></script>
   <!-- Custom scripts-->
-  <script type="text/javascript" src="js/main.js"></script>
+  <script <?php echo js_loader("main"); ?>></script>
 
 
 </html>
