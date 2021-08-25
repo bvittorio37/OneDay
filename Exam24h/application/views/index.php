e36379e03e7e8ee62516ead1dd2dfe7baa852637
bvittorio37 <67585804+bvittorio37@users.noreply.github.com>
2021-08-25 16:12:42 +0300

    index test

--- Exam24h/application/views/index.php
+++ Exam24h/application/views/index.php
@@ -23,7 +23,7 @@
                           <img src="<?php echo img_loader('biscuit','jpg'); ?>" class="card-img-top" alt="...">
                           <div class="card-body justify-content-center">
                           <h5 class="card-title text-center">Nom_poduit</h5>
-                          <h6 class="card-subtitle mb-2 text-muted text-center">200 $</h6>
+                          <h6 class="card-subtitle mb-2 text-muted text-center">200 Ar</h6>
                           <p class="pl-3"><a href="#" class="btn ml-5"><i class="fas fa-link"></i> Visit Site</a></p>
               
                           </div>
